4e86d9234513fce781cf1782ee7772e218e4707c
Ivan <cloud@coreup.io>
2023-12-18 00:44:56 +0300

    Update server and model download logic

--- src/download/index.ts
+++ src/download/index.ts
@@ -1,15 +1,48 @@
 import path from "node:path";
 import fs from "node:fs";
-import { Readable } from "stream";
-import { finished } from "stream/promises";
+import fsPromise from "node:fs/promises";
+import crypto from "node:crypto";
+import { Readable } from "node:stream";
+import { finished } from "node:stream/promises";
 import { ReadableStream } from "node:stream/web";
 
-export const getServerUrl = async (os: string) => {
+const checkFileExists = async (path: string) => {
+  try {
+    await fsPromise.access(path, fs.constants.F_OK);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const getCheckSum = async (path: string) => {
+  const file = await fsPromise.readFile(path);
+
+  const hash = crypto.createHash("sha256").update(file).digest("hex");
+
+  return hash;
+};
+
+export const getServerUrl = async (
+  os: string
+): Promise<{
+  url: string;
+  checksum: string;
+} | null> => {
+  // TODO: get latest version
   if (os === "win32") {
-    return "https://pub-ad9e0b7360bc4259878d0f81b89c5405.r2.dev/server.exe";
+    return {
+      url: "https://pub-ad9e0b7360bc4259878d0f81b89c5405.r2.dev/server.exe",
+      checksum:
+        "0a4c8e50c56f7a7c9d267e3e5f44115685e46e4a5f50456e6b28a23dbb46dcce",
+    };
   }
   if (os === "linux") {
-    return "https://pub-ad9e0b7360bc4259878d0f81b89c5405.r2.dev/server";
+    return {
+      url: "https://pub-ad9e0b7360bc4259878d0f81b89c5405.r2.dev/server",
+      checksum:
+        "2d242c88534a53f8576bf06f9afe23a2211d9a9424f6ef860047cb36d0db0956",
+    };
   }
 
   if (os === "darwin") {
@@ -19,41 +52,95 @@ export const getServerUrl = async (os: string) => {
   return null;
 };
 
-export const getModelUrl = async () => {
-  return "https://pub-ad9e0b7360bc4259878d0f81b89c5405.r2.dev/deepseek-coder-1.3b-base.Q8_0.gguf";
+export const getModelUrl = async (): Promise<{
+  url: string;
+  checksum: string;
+} | null> => {
+  // TODO: get latest version
+  return {
+    url: "https://pub-ad9e0b7360bc4259878d0f81b89c5405.r2.dev/deepseek-coder-1.3b-base.Q8_0.gguf",
+    checksum:
+      "9fcdcb283ef5b1d80ec7365b307c1ceab0c0f8ea079b49969f7febc06a11bccd",
+  };
 };
 
 export const downloadServer = async (extensionPath: string) => {
-  const os = process.platform;
+  console.log("Use default server");
 
-  const serverUrl = await getServerUrl(os);
-  if (serverUrl === null) {
-    return;
-  }
+  const os = process.platform;
 
   const serverPath = path.join(
     extensionPath,
     "server" + (os === "win32" ? ".exe" : "")
   );
-  const response = await fetch(serverUrl);
+
+  const serverFileInfo = await getServerUrl(os);
+
+  if (serverFileInfo === null) {
+    // TODO: throw error
+    return;
+  }
+
+  const isExists = await checkFileExists(serverPath);
+  if (isExists) {
+    const checkSum = await getCheckSum(serverPath);
+
+    if (checkSum === serverFileInfo.checksum) {
+      return serverPath;
+    }
+
+    console.log("Checksum server mismatch");
+  }
+
+  if (isExists) {
+    await fsPromise.unlink(serverPath);
+  }
+
+  const response = await fetch(serverFileInfo.url);
 
   const fileStream = fs.createWriteStream(serverPath, { flags: "wx" });
   await finished(
     Readable.fromWeb(response.body as ReadableStream<any>).pipe(fileStream)
   );
+
+  await fsPromise.chmod(serverPath, 0o755);
+
+  return serverPath;
 };
 
 export const downloadModel = async (extensionPath: string) => {
-  const modelUrl = await getModelUrl();
-  if (modelUrl === null) {
+  console.log("Use default model");
+
+  const modelPath = path.join(extensionPath, "model.gguf");
+
+  const modelFileInfo = await getModelUrl();
+
+  if (modelFileInfo === null) {
+    // TODO: throw error
     return;
   }
 
-  const modelPath = path.join(extensionPath, "model.gguf");
-  const response = await fetch(modelUrl);
+  const isExists = await checkFileExists(modelPath);
+
+  if (isExists) {
+    const checkSum = await getCheckSum(modelPath);
+
+    if (checkSum === modelFileInfo.checksum) {
+      return modelPath;
+    }
+    console.log("Checksum model mismatch");
+  }
+
+  if (isExists) {
+    await fsPromise.unlink(modelPath);
+  }
+
+  const response = await fetch(modelFileInfo.url);
 
   const fileStream = fs.createWriteStream(modelPath, { flags: "wx" });
   await finished(
     Readable.fromWeb(response.body as ReadableStream<any>).pipe(fileStream)
   );
+
+  return modelPath;
 };
--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import { ChildProcessWithoutNullStreams, spawn } from "child_process";
 import * as vscode from "vscode";
-import { downloadServer, downloadModel } from "./download";
+import { downloadServer, downloadModel, getCheckSum } from "./download";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -12,17 +12,22 @@ let server: ChildProcessWithoutNullStreams;
 export async function activate(context: vscode.ExtensionContext) {
   const fireCoderLog = vscode.window.createOutputChannel("FireCoder");
   fireCoderLog.append("FireCoder activated");
-  fireCoderLog.show();
-
-  fireCoderLog.append(context.extensionPath);
-
-  await downloadServer(context.extensionPath);
-  await downloadModel(context.extensionPath);
 
   const config = vscode.workspace.getConfiguration("firecoder");
-  const serverPath = config.get("serverPath") as string;
-  const modelPath = config.get("modelPath") as string;
-  if (serverPath !== "" && modelPath !== "") {
+
+  const serverPathCustom = config.get("serverPath") as string;
+  const serverPath =
+    serverPathCustom === ""
+      ? await downloadServer(context.extensionPath)
+      : serverPathCustom;
+
+  const modelPathCustom = config.get("modelPath") as string;
+  const modelPath =
+    modelPathCustom === ""
+      ? await downloadModel(context.extensionPath)
+      : modelPathCustom;
+
+  if (serverPath && modelPath) {
     server = spawn(serverPath, [`--model`, modelPath, "--port", "39129"], {
       detached: false,
     });
