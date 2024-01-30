adcfd94a9e54530c83b3458a1fd72fd0d9b30f2b
Ivan Pegashev <kj99001@gmail.com>
2024-01-30 14:36:54 +0300

    Update target to esnext in tsconfig.json, disable code chunks in build-react-no-split.js, add headers in sendCompletionRequest, use async generator function in chat, move isMacArm64 and isBaseModel variables to startServer function in server/index.ts, use sha256sum in getChecksum for Linux, and update handleHowdyClick function in App.tsx

--- src/common/chat/localChat.ts
+++ src/common/chat/localChat.ts
@@ -11,11 +11,19 @@ const logCompletion = (uuid = randomUUID() as string) => {
   };
 };
 
+class CustomEvent extends Event {
+  detail: any;
+  constructor(message: any, data: any) {
+    super(message, data);
+    this.detail = data.detail;
+  }
+}
+
 const defualtParameters = {
-  stream: false,
+  stream: true,
   n_predict: 128,
   temperature: 0.3,
-  stop: ["\n"],
+  stop: [],
   repeat_last_n: 256,
   repeat_penalty: 1.18,
   penalize_nl: false,
@@ -34,12 +42,12 @@ const defualtParameters = {
   slot_id: -1,
 };
 
-export const sendChatRequest = async (
+export async function* sendChatRequest(
   prompt: string,
   parameters: Record<string, any>,
   uuid: string,
   url: string
-) => {
+) {
   const { stopTask } = statusBar.startTask();
 
   const loggerCompletion = logCompletion(uuid);
@@ -54,53 +62,82 @@ export const sendChatRequest = async (
 
     const startTime = performance.now();
 
-    const response = await fetch(`${url}/completion`, {
-      body: JSON.stringify(parametersForCompletion),
-      method: "POST",
-    });
-
-    if (!response.ok) {
-      Logger.error(response.statusText);
-
-      vscode.window.showErrorMessage(
-        `Error: ${response.status} ${response.statusText}`
-      );
-      return null;
+    // const response = await fetch(`${url}/completion`, {
+    //   body: JSON.stringify(parametersForCompletion),
+    //   method: "POST",
+    //   headers: {
+    //     Connection: "keep-alive",
+    //     "Content-Type": "application/json",
+    //     Accept: "text/event-stream",
+    //   },
+    // });
+    let content = "";
+    let timings;
+    for await (const chunk of llama(prompt, parametersForCompletion, { url })) {
+      // @ts-ignore
+      if (chunk.data) {
+        // @ts-ignore
+        content += chunk.data.content;
+        yield content;
+      }
+      // @ts-ignore
+      // if (chunk.data.generation_settings) {
+      //   eventTarget.dispatchEvent(
+      //     new CustomEvent("generation_settings", {
+      //       // @ts-ignore
+      //       detail: chunk.data.generation_settings,
+      //     })
+      //   );
+      // }
+      // @ts-ignore
+      if (chunk.data.timings) {
+        // @ts-ignore
+        timings = chunk.data.timings;
+      }
     }
 
-    const completion: {
-      content: string;
-      slot_id: number;
-      timings: {
-        predicted_ms: number;
-        predicted_n: number;
-        predicted_per_second: number;
-        predicted_per_token_ms: number;
-        prompt_ms: number;
-        prompt_n: number;
-        prompt_per_second: number;
-        prompt_per_token_ms: number;
-      };
-    } = (await response.json()) as any;
+    // if (!response.ok) {
+    //   Logger.error(response.statusText);
 
-    if (completion.content === "") {
-      return null;
-    }
+    //   vscode.window.showErrorMessage(
+    //     `Error: ${response.status} ${response.statusText}`
+    //   );
+    //   return null;
+    // }
+
+    // const completion: {
+    //   content: string;
+    //   slot_id: number;
+    //   timings: {
+    //     predicted_ms: number;
+    //     predicted_n: number;
+    //     predicted_per_second: number;
+    //     predicted_per_token_ms: number;
+    //     prompt_ms: number;
+    //     prompt_n: number;
+    //     prompt_per_second: number;
+    //     prompt_per_token_ms: number;
+    //   };
+    // } = (await response.json()) as any;
+
+    // if (completion.content === "") {
+    //   return null;
+    // }
 
     loggerCompletion.info(
-      `Slot Id: ${completion.slot_id}; ` +
-        `Total time: ${(performance.now() - startTime).toFixed(2)}; ` +
-        `PP: ${completion?.timings?.prompt_per_second?.toFixed(2)}; [t/s] ` +
-        `TG: ${completion?.timings?.predicted_per_second?.toFixed(2)}; [t/s]`
+      // `Slot Id: ${completion.slot_id}; ` +
+      `Total time: ${(performance.now() - startTime).toFixed(2)}; ` +
+        `PP: ${timings?.prompt_per_second?.toFixed(2)}; [t/s] ` +
+        `TG: ${timings?.predicted_per_second?.toFixed(2)}; [t/s]`
     );
 
-    const timing = completion?.timings?.prompt_ms
-      ? completion?.timings?.prompt_ms
-      : performance.now() - startTime;
+    // const timing = completion?.timings?.prompt_ms
+    //   ? completion?.timings?.prompt_ms
+    //   : performance.now() - startTime;
 
     loggerCompletion.info("Request: finished");
-
-    return { content: completion.content, timing } as const;
+    return;
+    // return { content: completion.content } as const;
   } catch (error) {
     const Error = error as Error;
     if (Error.name === "AbortError") {
@@ -115,4 +152,171 @@ export const sendChatRequest = async (
   } finally {
     stopTask();
   }
+}
+
+const paramDefaults = {
+  stream: true,
+  n_predict: 500,
+  temperature: 0.2,
+  // stop: ["</s>"],
+};
+
+export async function* llama(
+  prompt: string,
+  params = {},
+  config: { controller?: AbortController; url?: string } = {}
+) {
+  let generation_settings;
+  let controller = config.controller;
+
+  if (!controller) {
+    controller = new AbortController();
+  }
+
+  const completionParams = { ...paramDefaults, ...params, prompt };
+
+  const response = await fetch(`${config.url}/completion`, {
+    method: "POST",
+    body: JSON.stringify(completionParams),
+    headers: {
+      Connection: "keep-alive",
+      "Content-Type": "application/json",
+      Accept: "text/event-stream",
+    },
+    // signal: controller.signal,
+  });
+  // @ts-ignore
+  const reader = response.body.getReader<any>();
+  const decoder = new TextDecoder();
+
+  let content = "";
+  let leftover = ""; // Buffer for partially read lines
+
+  try {
+    let cont = true;
+
+    while (cont) {
+      const result = await reader.read();
+      if (result.done) {
+        break;
+      }
+
+      // Add any leftover data to the current chunk of data
+      const text = leftover + decoder.decode(result.value);
+
+      // Check if the last character is a line break
+      const endsWithLineBreak = text.endsWith("\n");
+
+      // Split the text into lines
+      let lines = text.split("\n");
+
+      // If the text doesn't end with a line break, then the last line is incomplete
+      // Store it in leftover to be added to the next chunk of data
+      if (!endsWithLineBreak) {
+        // @ts-ignore
+        leftover = lines.pop();
+      } else {
+        leftover = ""; // Reset leftover if we have a line break at the end
+      }
+
+      // Parse all sse events and add them to result
+      const regex = /^(\S+):\s(.*)$/gm;
+      for (const line of lines) {
+        const match = regex.exec(line);
+        if (match) {
+          // @ts-ignore
+          result[match[1]] = match[2];
+          // since we know this is llama.cpp, let's just decode the json in data
+          // @ts-ignore
+          if (result.data) {
+            // @ts-ignore
+            result.data = JSON.parse(result.data);
+            // @ts-ignore
+            content += result.data.content;
+
+            // yield
+            yield result;
+
+            // if we got a stop token from server, we will break here
+            // @ts-ignore
+            if (result.data.stop) {
+              // @ts-ignore
+              if (result.data.generation_settings) {
+                // @ts-ignore
+                generation_settings = result.data.generation_settings;
+              }
+              cont = false;
+              break;
+            }
+          }
+          // @ts-ignore
+          if (result.error) {
+            // @ts-ignore
+            result.error = JSON.parse(result.error);
+            // @ts-ignore
+            if (result.error.content.includes("slot unavailable")) {
+              // Throw an error to be caught by upstream callers
+              throw new Error("slot unavailable");
+            } else {
+              // @ts-ignore
+              console.error(`llama.cpp error: ${result.error.content}`);
+            }
+          }
+          // @ts-ignore
+          if (result.error) {
+            // @ts-ignore
+            result.error = JSON.parse(result.error);
+            // @ts-ignore
+            console.error(`llama.cpp error: ${result.error.content}`);
+          }
+        }
+      }
+    }
+  } catch (e) {
+    // @ts-ignore
+    if (e.name !== "AbortError") {
+      console.error("llama error: ", e);
+    }
+    throw e;
+  } finally {
+    controller.abort();
+  }
+
+  return content;
+}
+
+export const llamaEventTarget = (prompt: string, params = {}, config = {}) => {
+  const eventTarget = new EventTarget();
+  (async () => {
+    let content = "";
+    for await (const chunk of llama(prompt, params, config)) {
+      // @ts-ignore
+      if (chunk.data) {
+        // @ts-ignore
+        content += chunk.data.content;
+        eventTarget.dispatchEvent(
+          // @ts-ignore
+          new CustomEvent("message", { detail: chunk.data })
+        );
+      }
+      // @ts-ignore
+      if (chunk.data.generation_settings) {
+        eventTarget.dispatchEvent(
+          new CustomEvent("generation_settings", {
+            // @ts-ignore
+            detail: chunk.data.generation_settings,
+          })
+        );
+      }
+      // @ts-ignore
+      if (chunk.data.timings) {
+        eventTarget.dispatchEvent(
+          // @ts-ignore
+          new CustomEvent("timings", { detail: chunk.data.timings })
+        );
+      }
+    }
+    eventTarget.dispatchEvent(new CustomEvent("done", { detail: { content } }));
+  })();
+  return eventTarget;
 };
--- webviews/src/utilities/vscode.ts
+++ webviews/src/utilities/vscode.ts
@@ -1,22 +1,10 @@
-// FROM: https://github.com/microsoft/vscode-webview-ui-toolkit-samples/tree/main/frameworks/hello-world-react-cra
 import type { WebviewApi } from "vscode-webview";
 
-/**
- * A utility wrapper around the acquireVsCodeApi() function, which enables
- * message passing and state management between the webview and extension
- * contexts.
- *
- * This utility also enables webview code to be run in a web browser-based
- * dev server by using native web browser features that mock the functionality
- * enabled by acquireVsCodeApi.
- */
 class VSCodeAPIWrapper {
   private readonly vsCodeApi: WebviewApi<unknown> | undefined;
   private messageCallback: Record<string, any> = {};
 
   constructor() {
-    // Check if the acquireVsCodeApi function exists in the current development
-    // context (i.e. VS Code development window or web browser)
     if (typeof acquireVsCodeApi === "function") {
       this.vsCodeApi = acquireVsCodeApi();
     }
@@ -33,14 +21,6 @@ class VSCodeAPIWrapper {
     });
   }
 
-  /**
-   * Post a message (i.e. send arbitrary data) to the owner of the webview.
-   *
-   * @remarks When running webview code inside a web browser, postMessage will instead
-   * log the given message to the console.
-   *
-   * @param message Abitrary data (must be JSON serializable) to send to the extension context.
-   */
   public async postMessage(message: { type: string; data: any }) {
     if (this.vsCodeApi) {
       // @ts-ignore
@@ -49,49 +29,43 @@ class VSCodeAPIWrapper {
         ...message,
         messageId,
       });
-      return this.response(messageId);
+      return await this.response(messageId);
     } else {
       console.log(message);
     }
   }
 
-  /**
-   * Get the persistent state stored for this webview.
-   *
-   * @remarks When running webview source code inside a web browser, getState will retrieve state
-   * from local storage (https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage).
-   *
-   * @return The current state or `undefined` if no state has been set.
-   */
-  public getState(): unknown | undefined {
-    if (this.vsCodeApi) {
-      return this.vsCodeApi.getState();
-    } else {
-      const state = localStorage.getItem("vscodeState");
-      return state ? JSON.parse(state) : undefined;
+  private async *responseGenerator(messageId: string) {
+    while (true) {
+      // @ts-ignore
+      const message = yield;
+      if (message?.data?.messageId === messageId) {
+        debugger;
+        yield message.data;
+      }
     }
   }
 
-  /**
-   * Set the persistent state stored for this webview.
-   *
-   * @remarks When running webview source code inside a web browser, setState will set the given
-   * state using local storage (https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage).
-   *
-   * @param newState New persisted state. This must be a JSON serializable object. Can be retrieved
-   * using {@link getState}.
-   *
-   * @return The new state.
-   */
-  public setState<T extends unknown | undefined>(newState: T): T {
+  public async postMessageGenerator(
+    message: { type: string; data: any },
+    messageCallback: (message: any) => void
+  ) {
     if (this.vsCodeApi) {
-      return this.vsCodeApi.setState(newState);
+      window.addEventListener("message", (message) => {
+        if (message?.data?.messageId === messageId) {
+          messageCallback(message?.data);
+        }
+      });
+      // @ts-ignore
+      const messageId = global.crypto.randomUUID();
+      this.vsCodeApi.postMessage({
+        ...message,
+        messageId,
+      });
     } else {
-      localStorage.setItem("vscodeState", JSON.stringify(newState));
-      return newState;
+      console.log(message);
     }
   }
 }
 
-// Exports class singleton to prevent multiple invocations of acquireVsCodeApi.
 export const vscode = new VSCodeAPIWrapper();
